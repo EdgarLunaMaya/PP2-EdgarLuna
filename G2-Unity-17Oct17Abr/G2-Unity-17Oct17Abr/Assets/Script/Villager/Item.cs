bdc3e097c242e0d3fbeac35e4f5c6ad97ecc9c6f
EdgarLunaMaya <144072162+EdgarLunaMaya@users.noreply.github.com>
2023-09-25 17:04:11 -0600

    Titulo-final

--- G2-Unity-17Oct17Abr/G2-Unity-17Oct17Abr/Assets/Script/Villager/Item.cs
+++ G2-Unity-17Oct17Abr/G2-Unity-17Oct17Abr/Assets/Script/Villager/Item.cs
@@ -1,15 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
-public class UI_Box_Background_Container_Content : MonoBehaviour
+public class Item : MonoBehaviour
 {
-    public TMP_Text content;
     // Start is called before the first frame update
     void Start()
     {
-        content = GetComponent<TMP_Text>();
+        
     }
 
     // Update is called once per frame
---
+++ G2-Unity-17Oct17Abr/G2-Unity-17Oct17Abr/Assets/Script/Villager/VillagerBase.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public enum VillagerType
+//{
+//    Normal = 0,
+//    Pescador = 1,
+//    Herrero = 2
+//}
+
+//public class VillagerBaase : MonoBehaviour
+//{
+//    public string Name = "Villager";
+//    public int health = 100;
+//    public VillagerType type;
+//    public Sprite sikin;
+//    public Item item;
+//    public List<Item> inventory;
+
+//    public virtual void AddItemToInventory(Item _item)
+
+//    public virtual string getName()
+
+//    public virtual void setName(string _name)
+//    {
+//    Name = _name;
+//    }
+
+//    public virtual void setHealth(int _health)
+//    {
+//        health = _health;
+//    }
+
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+        
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+        
+//    }
+//}
